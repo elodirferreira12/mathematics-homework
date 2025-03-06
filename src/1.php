91289b9bdd57e41bb67a2c68470ff489f627b0dd
elodirferreira12 <elodirferreira12@gmail.com>
2025-03-06 15:16:51 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,12 @@
+  <?php
+
+// Calculate and output the sum of two numbers
+$num1 = 5;
+$num2 = 7;
+echo $num1 + $num2;
+
+// Output a greeting message with a variable name
+$name = "John";
+echo "Hello, " . $name . "!";
+
+?>

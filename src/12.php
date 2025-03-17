64674e7d61b813892f3c03444d762d550a4e2b42
elodirferreira12 <elodirferreira12@gmail.com>
2025-03-17 15:29:28 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,14 @@
+<?php
+
+// Define a function to calculate the area of a rectangle
+function getArea($length, $width) {
+  return $length * $width;
+}
+
+// Use the function to calculate the area of a rectangle with length 5 and width 3
+$area = getArea(5, 3);
+
+// Print the result
+echo "The area of the rectangle is $area";
+
+?>

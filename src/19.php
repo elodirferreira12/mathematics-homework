158c9fc93f88860cd7f95844a3272085df014f30
elodirferreira12 <elodirferreira12@gmail.com>
2025-03-24 21:21:13 +0000

    Add 19.php

---
+++ src/19.php
@@ -0,0 +1,10 @@
+<?php
+// Define a variable to store the initial conditions
+$initialConditions = array(1, 2, 3);
+
+// Perform some calculations using the variables defined in the initialConditions array
+$result = $initialConditions[0] * $initialConditions[1] - $initialConditions[2];
+
+// Print the result
+echo "The result of the calculation is: " . $result;
+?>

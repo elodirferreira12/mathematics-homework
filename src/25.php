f48ee34c36e58c6bae59c07eda1599c761f5a652
elodirferreira12 <elodirferreira12@gmail.com>
2025-04-05 05:53:15 +0000

    Add 25.php

---
+++ src/25.php
@@ -0,0 +1,13 @@
+<?php
+// Define variables and constants
+$vars = array(
+  "variable1" => "value1",
+  "variable2" => "value2"
+);
+
+// Perform some operation with variables
+$result = $vars["variable1"] + $vars["variable2"];
+
+// Display the result
+echo "The sum of variable1 and variable2 is: " . $result;
+?>

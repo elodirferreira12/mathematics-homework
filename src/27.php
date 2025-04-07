365c9951183231e7d55e53869b41ccf6db1a3fbc
elodirferreira12 <elodirferreira12@gmail.com>
2025-04-07 05:00:28 +0000

    Add 27.php

---
+++ src/27.php
@@ -0,0 +1,10 @@
+<?php
+// Define your mathematical problem here
+$problem = "Please solve the following equation: 2x + 3 = 7";
+
+// Perform algebraic operations on the given problem
+$x = (7 - 3) / 2;
+
+// Display the solution to the user
+echo "The value of x is $x.";
+?>

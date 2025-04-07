1af7ec36860ce6cea142a5705659afca3a2f3fda
elodirferreira12 <elodirferreira12@gmail.com>
2025-04-07 05:32:29 +0000

    Add 31.php

---
+++ src/31.php
@@ -0,0 +1,13 @@
+<?php
+// Example PHP code to demonstrate a basic arithmetic operation
+$x = 5;
+$y = 3;
+
+// Perform an addition operation
+$result = $x + $y;
+echo "The sum of $x and $y is: $result";
+
+// Perform another addition operation (this will throw an error in PHP)
+// Uncommenting this line will cause a syntax error
+// $z = $x + $y; // This will result in a compile-time error
+?>

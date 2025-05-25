7a8a885764892c013b506ac2a810afcfccd7e90d
elodirferreira12 <elodirferreira12@gmail.com>
2025-05-25 15:53:57 +0000

    Add 49.php

---
+++ src/49.php
@@ -0,0 +1,10 @@
+<?php
+// Define an array of numbers to sort
+numbers = [7, 3, 5, 8, 1];
+
+// Use the built-in `sort` function to sort the array in ascending order
+sort($numbers);
+
+// Output the sorted array
+print_r($numbers);
+?>

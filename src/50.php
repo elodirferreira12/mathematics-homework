c1d90877514a95c7ee9c2bbc96a508d538ef21df
elodirferreira12 <elodirferreira12@gmail.com>
2025-05-26 22:37:50 +0000

    Add 50.php

---
+++ src/50.php
@@ -0,0 +1,11 @@
+<?php
+// Example PHP script with mathematical calculations and variable assignments.
+// This is just an example to demonstrate how you might structure such a script.
+
+$pi = 3.14159;
+$a = 2;
+$b = $a + 5;
+
+echo "The value of pi is: " . $pi;
+echo "Sum of $a and $b: " . $b;
+?>
